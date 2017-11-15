1cd7b94adc747c69c1565ce889578688f02d428a
Denis <vite0150@gmail.com>
2017-11-15 15:46:53 -0600

    Clases koko comparacion

---
+++ src/kotlin/ast/KoKoEqs.kt
@@ -0,0 +1,26 @@
+/*
+  @author Denis Rodriguez Viquez
+          Luis Vasquez Quiros
+          Walter Chavez Oviedo
+  @since 2017
+*/
+package kokoslan.kotlin.ast;
+
+import java.util.*;
+import java.io.*;
+
+
+class KoKoEQS(operator : KoKoAst, left : KoKoAst, right : KoKoAst) : KoKoBiOperation(operator, left, right) {
+	
+	override fun eval(ctx : KoKoContext): KoKoValue{
+	    try {
+		    val lv = left().eval(ctx)
+			val rv = right().eval(ctx)
+	        if(lv is KoKoNumValue)
+     			return KoKoBoolValue((lv as KoKoNumValue).value != (rv as KoKoNumValue).value)
+     		return KoKoBoolValue((lv as KoKoBoolValue).value != (rv as KoKoBoolValue).value)
+	   } catch ( e : Exception ) {
+			throw KoKoEvalException("${e.message}")
+	   }
+    }
+}
