2a3a01a450db57c66d956efd1c746f2683c5c230
Denis <vite0150@gmail.com>
2017-11-16 04:14:04 -0600

    comentarios

--- src/kotlin/compiler/KoKoEmiter.kt
+++ src/kotlin/compiler/KoKoEmiter.kt
@@ -1,3 +1,9 @@
+/*
+  @author Denis Rodriguez Viquez
+          Luis Vasquez Quiros
+          Walter Chavez Oviedo
+  @since 2017
+*/
 package kokoslan.kotlin.compile;
 
 import kokoslan.ast.KoKoElvis
